eca64241c11c09a43f1e4858b5f2304de903fd28
Tyler Morgan <tylermorgan86@gmail.com>
2013-10-25 14:19:04 -0700

    post crud almost finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def show
-
+    #
   end
 
   def new
@@ -18,27 +18,39 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.save
       flash[:notice] = "Post has been created"
-      redirect_to @post
+      redirect_to :back
     else
       flash[:alert] = "Could not create post"
-      redirect_to posts_path
+      render action: :new
     end
   end
 
   def edit
-
+    #
   end
 
   def update
-
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post successfully updated"
+      redirect_to @post
+    else
+      flash[:alert] = "Could not update post"
+      render action: :edit
+    end
   end
 
   def destroy
-
+    @post.destroy
+    flash[:notice] = "Post has been deleted"
+    redirect_to posts_path
   end
 
 private
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  
+  validates_presence_of :title
+  validates_uniqueness_of :title
+
+  validates_presence_of :content
 end
